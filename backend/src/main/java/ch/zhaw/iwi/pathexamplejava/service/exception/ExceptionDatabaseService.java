a0d9607d7beb930b8143f76e4e1ffc29148791cf
Daniel <zhaw-metzidan@students.zhaw.ch>
2019-04-24 16:23:55 +0200

    jcenter library added
    moved some classes in the path library. other classes access those at jcenter bintray

--- backend/src/main/java/ch/zhaw/iwi/pathexamplejava/service/exception/ExceptionDatabaseService.java
+++
@@ -1,7 +0,0 @@
-package ch.zhaw.iwi.pathexamplejava.service.exception;
-
-import ch.zhaw.iwi.pathexamplejava.service.AbstractDatabaseService;
-
-public class ExceptionDatabaseService extends AbstractDatabaseService {
-
-}
